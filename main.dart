86962abcc68458c78037047157ce53975d922176
MohammadYahfoufi <mhmdyahfoufi52@gmail.com>
2024-08-21 16:48:34 +0300

    Implemented null safety in a Dart application

--- main.dart
+++ main.dart
@@ -2,14 +2,18 @@ void main() {
   String name = "";
   String coffee = "Coffee";
   String? milk;
+  String milk2 = milk ?? "Dairy";
   print(isEmpty(name));
   //print(isEmpty(null));
   makeCoffee(coffee, milk);
   printLen(milk);
   realString(coffee);
   milk = 'Dairy';
-  notNullable(coffee);
   notNullable(milk);
+  notNullable(milk2);
+
+  Person p = Person();
+  print('Name: ${p.name ?? "Mohammad"} and age: ${p.address ?? "Yahfoufi"}');
 }
 
 bool isEmpty(String string) => string.length == 0;
@@ -31,4 +35,9 @@ void realString(String definitelyString) {
 
 void notNullable(String? string) {
   print(string!.length);
+}
+
+class Person {
+  String? name;
+  String? address;
 }
